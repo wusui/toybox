b28e1157e20470f5fec5fe38a406f003d1beb030
Warren Usui <wusui@users.noreply.github.com>
2016-05-09 00:27:08 -0700

    Fix year reference, lower free agents checked

--- src/baseball/get_pool.py
+++ src/baseball/get_pool.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+from get_players import available;
+from html_strings import B_STATS
+from html_strings import P_STATS
 def get_pool(period, pos, league):
     """Get available players (as dictionary keys)
 
     Input:
-        period: length of time, in days or year number (7, 14, 30, 2016)
+        period: length of time, in days or year number (7, 14, 30, 20xx)
         pos: position of the players being found
         league: league number
 
@@ -16,7 +20,7 @@ def get_pool(period, pos, league):
     if pos.endswith('P'):
         statlist = P_STATS
     for stat in statlist:
-        for count in range(0,4):
+        for count in range(0,2):
             data = available(period, pos, stat, league, count)
             for plr in data:
                 key = ":".join([plr['name'], plr['team'], plr['pos']])
