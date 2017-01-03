b5c0d2f5bbdd859681faed765cfceef16b9ecf58
Warren Usui <wusui@users.noreply.github.com>
2017-01-02 19:10:06 -0800

    Add wrapper.py
    Wrapper interface between gui related code and actual problem solving.

---
+++ src/chess/brains/wrapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from board import board
+
+def solver_filter(in_data):
+    cboard = board(in_data)
+    if len(cboard.msg_type) > 0:
+        return make_emesg(cboard)
+    return "ATTENTION|Chess solver not yet implemented|120|360"
+
+def make_emesg(cboard):
+    lilen = len(cboard.error_list) * 30 + 90
+    limax = 0
+    for msg in cboard.error_list:
+        if len(msg) > limax:
+            limax = len(msg)
+    limax = limax * 12 + 60
+    internal_list = "</li><li>".join(cboard.error_list)
+    bmsg = "<ul><li>%s</li></ul>" % internal_list
+    return "|".join([cboard.msg_type, bmsg, "%d" % lilen, "%d" % limax])
